40a5820d0aab793685ecb64b9847a04e737c9ad2
suneobbt <39382727+suneobbt@users.noreply.github.com>
2021-11-28 05:10:47 +0100

    Add files via upload

--- src/main/java/Control/GeneralConstants.java
+++ src/main/java/Control/GeneralConstants.java
@@ -1,33 +1,33 @@
 package Control;
 
 /**
- * GeneralConstants
- * Constants class
+ * GeneralConstants Constants class
+ * 
  * @author Suneo
  *
  */
 
 public class GeneralConstants {
-    protected final static String USER = "[Myself] > ";
-    protected final static String SERVER_INSTANCE_LONG= "SERVER";
-    protected final static String CLIENT_INSTANCE_LONG= "CLIENT";
-    protected final static String SERVER_INSTANCE_SHORT= "S";
-    protected final static String CLIENT_INSTANCE_SHORT= "C";
-    protected final static String LOCALHOST= "127.0.0.1";
-    protected final static String DEFAULT_PORT= "5050";  
-    
-    public class IsoLevel{   
-    	public final static String IL1 = "ISOLATION LEVEL [1]";
-        public final static String IL2 = "ISOLATION LEVEL [2]";
-        public final static String IL3 = "ISOLATION LEVEL [3]";
-        public final static String ILN = "NO ISOLATION LEVEL [N]";
+	protected final static String USER = "[Myself] > ";
+	protected final static String SERVER_INSTANCE_LONG = "SERVER";
+	protected final static String CLIENT_INSTANCE_LONG = "CLIENT";
+	protected final static String SERVER_INSTANCE_SHORT = "S";
+	protected final static String CLIENT_INSTANCE_SHORT = "C";
+	protected final static String LOCALHOST = "127.0.0.1";
+	protected final static String DEFAULT_PORT = "5050";
 
-    }
-       
-    public class Aesthetic{
+	public class IsoLevel {
+		public final static String IL1 = "ISOLATION LEVEL [1]";
+		public final static String IL2 = "ISOLATION LEVEL [2]";
+		public final static String IL3 = "ISOLATION LEVEL [3]";
+		public final static String ILN = "NO ISOLATION LEVEL [N]";
+
+	}
+
+	public class Aesthetic {
 		public final static String VERSION = "v1.2.2";
 		public final static String BUSINESS = "babot.cat";
-	    public static final String TITLE = "\n" +	                                     
+		public static final String TITLE = "\n" +                                     
 	    	"   _____                _           \r\n" + 
 	    	"  / ____|              | |          \r\n" + 
 	    	" | (___   ___ _ __   __| | ___ _ __ \r\n" + 
@@ -36,14 +36,14 @@ public class GeneralConstants {
 	    	" |_____/ \\___|_| |_|\\__,_|\\___|_|   " + BUSINESS + " \r\n" + 
 	    	"                                    \r\n" + 
 	    	"                                    \r\n";	                                     
-    }
+	}
     
-    public class CryptoConstants{
-    	public final static String ALGORITHM_LONG = "AES/ECB/PKCS5Padding";
-    	public final static String ALGORITHM_SHORT = "AES";
-    }
-    
-    public class Commands {
-    	protected final static String END_CHAT = "/kill";   	
-    }
+	public class CryptoConstants {
+		public final static String ALGORITHM_LONG = "AES/ECB/PKCS5Padding";
+		public final static String ALGORITHM_SHORT = "AES";
+	}
+
+	public class Commands {
+		protected final static String END_CHAT = "/kill";
+	}
 }
